de039ac3719ebccb82312da4b1f242143f0faf7e
Harlow Foxworthy <22BKarolia@heckgrammar.co.uk>
2025-01-22 12:13:26 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace DiceGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            int die1 = rand.Next(1,7);
+            int die2 = rand.Next(1,7);
+            var score = 0;
+            if (die1 == die2) {
+                if (die1 == 6)
+                {
+                    score = 0;
+                }
+                else { 
+                    score = die1*2;
+                }
+            } else
+            {
+                score = die1+die2;
+            }
+            Console.WriteLine("Score: " + score);
+            int die3 = rand.Next(1, 7);
+            int die4 = rand.Next(1, 7);
+            var score2 = 0;
+            if (die3 == die4)
+            {
+                if (die1 == 6)
+                {
+                    score2 = 0;
+                }
+                else
+                {
+                    score2 = die3 * 2;
+                }
+            }
+            else
+            {
+                score2 = die3 + die4;
+            }
+            Console.WriteLine("Score: " + score2);
+        }
+    }
+}
